31bbbe2c2adc4c8346506cc6e6a676c2a6d530ac
Xenn765 <sonic-evo@hotmail.co.uk>
2024-10-24 08:48:29 +0100

    Update asurabus-hitboxes.lua
    Add hitbox offset fixes and adds afterimage hitboxes

--- hitboxes/asurabus-hitboxes.lua
+++ hitboxes/asurabus-hitboxes.lua
@@ -1,6 +1,5 @@
 local rb, rbs, rw, rws, rd = memory.readbyte, memory.readbytesigned, memory.readword, memory.readwordsigned, memory.readdword
 
-
 function P1() --P1 Info
 
 XPos_SCR=rws(0x004033CE)
@@ -8,6 +7,7 @@ YPos_SCR=rws(0x004033D0)
 Facing=rws(0x004033DA)
 ATKState=rws(0x004033F0)
 Char=rws(0x004039A6)
+Boost=rws(0x00403F56)
 
 A1_XR=rws(0x004033F6)
 A1_YT=rws(0x004033F8)
@@ -36,7 +36,7 @@ H2_XL=rws(0x00403518)
 H2_YB=rws(0x0040351A)
 
 -------------------------------------
--- Alice Stuff
+-- DMW/Afterimage Stuff
 DMW_XPos_SCR=rws(0x00404D42)
 DMW_YPos_SCR=rws(0x00404D44)
 DMW_Facing=rws(0x00404D4E)
@@ -49,6 +49,10 @@ DMW_A2_XR=rws(0x00404D72)
 DMW_A2_YT=rws(0x00404D74)
 DMW_A2_XL=rws(0x00404D76)
 DMW_A2_YB=rws(0x00404D78)
+DMW_C1_XR=rws(0x00404D7A)
+DMW_C1_YT=rws(0x00404D7C)
+DMW_C1_XL=rws(0x00404D7E)
+DMW_C1_YB=rws(0x00404D80)
 -------------------------------------
 
 -------------------------------------
@@ -65,9 +69,9 @@ if (P_XR or P_YT or P_XL or P_YB ~=0) and Facing == 1 then
 		0x00FF00FF)
 elseif (P_XR or P_YT or P_XL or P_YB ~=0) and Facing == 0 then
 		gui.box(
-		XPos_SCR+P_XR-1,
+		XPos_SCR+P_XR,
 		0+(YPos_SCR)+P_YT,
-		XPos_SCR+P_XL-1,
+		XPos_SCR+P_XL,
 		0+(YPos_SCR)+P_YB,
 		0x00FF0030,
 		0x00FF00FF)
@@ -84,9 +88,9 @@ if (H1_XR or H1_YT or H1_XL or H1_YB ~=0) and Facing == 1 then
 		0x7777FFFF)
 elseif (H1_XR or H1_YT or H1_XL or H1_YB ~=0) and Facing == 0 then
 		gui.box(
-		XPos_SCR+H1_XR-1,
+		XPos_SCR+H1_XR,
 		0+(YPos_SCR)+H1_YT,
-		XPos_SCR+H1_XL-1,
+		XPos_SCR+H1_XL,
 		0+(YPos_SCR)+H1_YB,
 		0x7777FF30,
 		0x7777FFFF)
@@ -103,9 +107,9 @@ if (H2_XR or H2_YT or H2_XL or H2_YB ~=0) and Facing == 1 then
 		0x7777FFFF)
 elseif (H2_XR or H2_YT or H2_XL or H2_YB ~=0) and Facing == 0 then
 		gui.box(
-		XPos_SCR+H2_XR-1,
+		XPos_SCR+H2_XR,
 		0+(YPos_SCR)+H2_YT,
-		XPos_SCR+H2_XL-1,
+		XPos_SCR+H2_XL,
 		0+(YPos_SCR)+H2_YB,
 		0x7777FF30,
 		0x7777FFFF)
@@ -114,18 +118,18 @@ end
 --Hitbox 1
 if ATKState > 0 and Facing == 1 then
 		gui.box(
-		XPos_SCR+A1_XR,
-		0+(YPos_SCR)+A1_YT,
-		XPos_SCR+A1_XL,
-		0+(YPos_SCR)+A1_YB,
+		XPos_SCR+A1_XR-1,
+		0+(YPos_SCR)+A1_YT-1,
+		XPos_SCR+A1_XL-1,
+		0+(YPos_SCR)+A1_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 elseif ATKState > 0 and Facing == 0 then
 		gui.box(
-		XPos_SCR+A1_XR-1,
-		0+(YPos_SCR)+A1_YT,
-		XPos_SCR+A1_XL-1,
-		0+(YPos_SCR)+A1_YB,
+		XPos_SCR+A1_XR,
+		0+(YPos_SCR)+A1_YT-1,
+		XPos_SCR+A1_XL,
+		0+(YPos_SCR)+A1_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 end
@@ -133,18 +137,18 @@ end
 --Hitbox 2
 if ATKState > 0 and Facing == 1 then
 		gui.box(
-		XPos_SCR+A2_XR,
-		0+(YPos_SCR)+A2_YT,
-		XPos_SCR+A2_XL,
-		0+(YPos_SCR)+A2_YB,
+		XPos_SCR+A2_XR-1,
+		0+(YPos_SCR)+A2_YT-1,
+		XPos_SCR+A2_XL-1,
+		0+(YPos_SCR)+A2_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 elseif ATKState > 0 and Facing == 0 then
 		gui.box(
-		XPos_SCR+A2_XR-1,
-		0+(YPos_SCR)+A2_YT,
-		XPos_SCR+A2_XL-1,
-		0+(YPos_SCR)+A2_YB,
+		XPos_SCR+A2_XR,
+		0+(YPos_SCR)+A2_YT-1,
+		XPos_SCR+A2_XL,
+		0+(YPos_SCR)+A2_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 end
@@ -152,18 +156,18 @@ end
 --Clash Hitbox
 if ATKState > 0 and Facing == 1 then
 		gui.box(
-		XPos_SCR+C1_XR,
-		0+(YPos_SCR)+C1_YT,
-		XPos_SCR+C1_XL,
-		0+(YPos_SCR)+C1_YB,
+		XPos_SCR+C1_XR-1,
+		0+(YPos_SCR)+C1_YT-1,
+		XPos_SCR+C1_XL-1,
+		0+(YPos_SCR)+C1_YB-1,
 		0xFFFF0030,
 		0xFFFF00FF)
 elseif ATKState > 0 and Facing == 0 then
 		gui.box(
-		XPos_SCR+C1_XR-1,
-		0+(YPos_SCR)+C1_YT,
-		XPos_SCR+C1_XL-1,
-		0+(YPos_SCR)+C1_YB,
+		XPos_SCR+C1_XR,
+		0+(YPos_SCR)+C1_YT-1,
+		XPos_SCR+C1_XL,
+		0+(YPos_SCR)+C1_YB-1,
 		0xFFFF0030,
 		0xFFFF00FF)
 end
@@ -175,49 +179,121 @@ gui.drawline(XPos_SCR,(-3+YPos_SCR), XPos_SCR, (3+YPos_SCR), 0xFFFFFFFF)
 -- Draw DMW Boxes
 
 --Hitbox 1
-if Char == 06 then --Are you Alice?
+if Char == 6 then --Are you Alice?
 	if DMW_ATKState > 0 and DMW_Facing == 1 then
 		gui.box(
-		DMW_XPos_SCR+DMW_A1_XR,
-		0+(DMW_YPos_SCR)+DMW_A1_YT,
-		DMW_XPos_SCR+DMW_A1_XL,
-		0+(DMW_YPos_SCR)+DMW_A1_YB,
+		DMW_XPos_SCR+DMW_A1_XR-1,
+		0+(DMW_YPos_SCR)+DMW_A1_YT-1,
+		DMW_XPos_SCR+DMW_A1_XL-1,
+		0+(DMW_YPos_SCR)+DMW_A1_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
 		gui.box(
-		DMW_XPos_SCR+DMW_A1_XR-1,
-		0+(DMW_YPos_SCR)+DMW_A1_YT,
-		DMW_XPos_SCR+DMW_A1_XL-1,
-		0+(DMW_YPos_SCR)+DMW_A1_YB,
+		DMW_XPos_SCR+DMW_A1_XR,
+		0+(DMW_YPos_SCR)+DMW_A1_YT-1,
+		DMW_XPos_SCR+DMW_A1_XL,
+		0+(DMW_YPos_SCR)+DMW_A1_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 	end
 end
 
 --Hitbox 2
-if Char == 06 then --Are you Alice?
+if Char == 6 then --Are you Alice?
 	if DMW_ATKState > 0 and DMW_Facing == 1 then
 		gui.box(
-		DMW_XPos_SCR+DMW_A2_XR,
-		0+(DMW_YPos_SCR)+DMW_A2_YT,
-		DMW_XPos_SCR+DMW_A2_XL,
-		0+(DMW_YPos_SCR)+DMW_A2_YB,
+		DMW_XPos_SCR+DMW_A2_XR-1,
+		0+(DMW_YPos_SCR)+DMW_A2_YT-1,
+		DMW_XPos_SCR+DMW_A2_XL-1,
+		0+(DMW_YPos_SCR)+DMW_A2_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
 		gui.box(
-		DMW_XPos_SCR+DMW_A2_XR-1,
-		0+(DMW_YPos_SCR)+DMW_A2_YT,
-		DMW_XPos_SCR+DMW_A2_XL-1,
-		0+(DMW_YPos_SCR)+DMW_A2_YB,
+		DMW_XPos_SCR+DMW_A2_XR,
+		0+(DMW_YPos_SCR)+DMW_A2_YT-1,
+		DMW_XPos_SCR+DMW_A2_XL,
+		0+(DMW_YPos_SCR)+DMW_A2_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 	end
 end
 
 --DMW Axis
-if Char == 0x06 then --Are you Alice?
+if Char == 6 then --Are you Alice?
+gui.drawline((DMW_XPos_SCR - 3),DMW_YPos_SCR, (DMW_XPos_SCR + 3), DMW_YPos_SCR, 0xFFFFFFFF)
+gui.drawline(DMW_XPos_SCR,(-3+DMW_YPos_SCR), DMW_XPos_SCR, (3+DMW_YPos_SCR), 0xFFFFFFFF)
+end
+
+-------------------------------------
+-- Draw Afterimage Boxes
+
+--Hitbox 1
+if (Char == 0 or Char == 3 or Char == 8) and Boost == 1 then --Are you Alice?
+	if DMW_ATKState > 0 and DMW_Facing == 1 then
+		gui.box(
+		DMW_XPos_SCR+DMW_A1_XR-1,
+		0+(DMW_YPos_SCR)+DMW_A1_YT-1,
+		DMW_XPos_SCR+DMW_A1_XL-1,
+		0+(DMW_YPos_SCR)+DMW_A1_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
+		gui.box(
+		DMW_XPos_SCR+DMW_A1_XR,
+		0+(DMW_YPos_SCR)+DMW_A1_YT-1,
+		DMW_XPos_SCR+DMW_A1_XL,
+		0+(DMW_YPos_SCR)+DMW_A1_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	end
+end
+
+--Hitbox 2
+if (Char == 0 or Char == 3 or Char == 8) and Boost == 1 then --Are you Alice?
+	if DMW_ATKState > 0 and DMW_Facing == 1 then
+		gui.box(
+		DMW_XPos_SCR+DMW_A2_XR-1,
+		0+(DMW_YPos_SCR)+DMW_A2_YT-1,
+		DMW_XPos_SCR+DMW_A2_XL-1,
+		0+(DMW_YPos_SCR)+DMW_A2_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
+		gui.box(
+		DMW_XPos_SCR+DMW_A2_XR,
+		0+(DMW_YPos_SCR)+DMW_A2_YT-1,
+		DMW_XPos_SCR+DMW_A2_XL,
+		0+(DMW_YPos_SCR)+DMW_A2_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	end
+end
+
+--Clashbox
+if (Char == 0 or Char == 3 or Char == 8) and Boost == 1 then --Are you Alice?
+	if DMW_ATKState > 0 and DMW_Facing == 1 then
+		gui.box(
+		DMW_XPos_SCR+DMW_C1_XR-1,
+		0+(DMW_YPos_SCR)+DMW_C1_YT-1,
+		DMW_XPos_SCR+DMW_C1_XL-1,
+		0+(DMW_YPos_SCR)+DMW_C1_YB-1,
+		0xFFFF0030,
+		0xFFFF00FF)
+	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
+		gui.box(
+		DMW_XPos_SCR+DMW_C1_XR,
+		0+(DMW_YPos_SCR)+DMW_C1_YT-1,
+		DMW_XPos_SCR+DMW_C1_XL,
+		0+(DMW_YPos_SCR)+DMW_C1_YB-1,
+		0xFFFF0030,
+		0xFFFF00FF)
+	end
+end
+
+--Afterimage Axis
+if (Char == 0 or Char == 3 or Char == 8) and Boost == 1 then --Are you Alice?
 gui.drawline((DMW_XPos_SCR - 3),DMW_YPos_SCR, (DMW_XPos_SCR + 3), DMW_YPos_SCR, 0xFFFFFFFF)
 gui.drawline(DMW_XPos_SCR,(-3+DMW_YPos_SCR), DMW_XPos_SCR, (3+DMW_YPos_SCR), 0xFFFFFFFF)
 end
@@ -233,6 +309,7 @@ YPos_SCR=rws(0x00404086)
 Facing=rws(0x00404090)
 ATKState=rws(0x004040A8)
 Char=rws(0x00404666)
+Boost=rws(0x00404C16)
 
 A1_XR=rws(0x004040AE)
 A1_YT=rws(0x004040B0)
@@ -261,7 +338,7 @@ H2_XL=rws(0x004041D2)
 H2_YB=rws(0x004041D4)
 
 -------------------------------------
--- Alice Stuff
+-- DMW/Afterimage Stuff
 DMW_XPos_SCR=rws(0x004059E2)
 DMW_YPos_SCR=rws(0x004059E4)
 DMW_Facing=rws(0x004059EE)
@@ -274,6 +351,10 @@ DMW_A2_XR=rws(0x00405A12)	--Hitbox 2
 DMW_A2_YT=rws(0x00405A14)
 DMW_A2_XL=rws(0x00405A16)
 DMW_A2_YB=rws(0x00405A18)
+DMW_C1_XR=rws(0x00405A1A)	--Hitbox 2
+DMW_C1_YT=rws(0x00405A1C)
+DMW_C1_XL=rws(0x00405A1E)
+DMW_C1_YB=rws(0x00405A20)
 -------------------------------------
 
 -------------------------------------
@@ -290,9 +371,9 @@ if (P_XR or P_YT or P_XL or P_YB ~=0) and Facing == 1 then
 		0x00FF00FF)
 elseif (P_XR or P_YT or P_XL or P_YB ~=0) and Facing == 0 then
 		gui.box(
-		XPos_SCR+P_XR-1,
+		XPos_SCR+P_XR,
 		0+(YPos_SCR)+P_YT,
-		XPos_SCR+P_XL-1,
+		XPos_SCR+P_XL,
 		0+(YPos_SCR)+P_YB,
 		0x00FF0030,
 		0x00FF00FF)
@@ -309,9 +390,9 @@ if (H1_XR or H1_YT or H1_XL or H1_YB ~=0) and Facing == 1 then
 		0x7777FFFF)
 elseif (H1_XR or H1_YT or H1_XL or H1_YB ~=0) and Facing == 0 then
 		gui.box(
-		XPos_SCR+H1_XR-1,
+		XPos_SCR+H1_XR,
 		0+(YPos_SCR)+H1_YT,
-		XPos_SCR+H1_XL-1,
+		XPos_SCR+H1_XL,
 		0+(YPos_SCR)+H1_YB,
 		0x7777FF30,
 		0x7777FFFF)
@@ -328,9 +409,9 @@ if (H2_XR or H2_YT or H2_XL or H2_YB ~=0) and Facing == 1 then
 		0x7777FFFF)
 elseif (H2_XR or H2_YT or H2_XL or H2_YB ~=0) and Facing == 0 then
 		gui.box(
-		XPos_SCR+H2_XR-1,
+		XPos_SCR+H2_XR,
 		0+(YPos_SCR)+H2_YT,
-		XPos_SCR+H2_XL-1,
+		XPos_SCR+H2_XL,
 		0+(YPos_SCR)+H2_YB,
 		0x7777FF30,
 		0x7777FFFF)
@@ -339,18 +420,18 @@ end
 --Hitbox 1
 if ATKState > 0 and Facing == 1 then
 		gui.box(
-		XPos_SCR+A1_XR,
-		0+(YPos_SCR)+A1_YT,
-		XPos_SCR+A1_XL,
-		0+(YPos_SCR)+A1_YB,
+		XPos_SCR+A1_XR-1,
+		0+(YPos_SCR)+A1_YT-1,
+		XPos_SCR+A1_XL-1,
+		0+(YPos_SCR)+A1_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 elseif ATKState > 0 and Facing == 0 then
 		gui.box(
-		XPos_SCR+A1_XR-1,
-		0+(YPos_SCR)+A1_YT,
-		XPos_SCR+A1_XL-1,
-		0+(YPos_SCR)+A1_YB,
+		XPos_SCR+A1_XR,
+		0+(YPos_SCR)+A1_YT-1,
+		XPos_SCR+A1_XL,
+		0+(YPos_SCR)+A1_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 end
@@ -358,18 +439,18 @@ end
 --Hitbox 2
 if ATKState > 0 and Facing == 1 then
 		gui.box(
-		XPos_SCR+A2_XR,
-		0+(YPos_SCR)+A2_YT,
-		XPos_SCR+A2_XL,
-		0+(YPos_SCR)+A2_YB,
+		XPos_SCR+A2_XR-1,
+		0+(YPos_SCR)+A2_YT-1,
+		XPos_SCR+A2_XL-1,
+		0+(YPos_SCR)+A2_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 elseif ATKState > 0 and Facing == 0 then
 		gui.box(
-		XPos_SCR+A2_XR-1,
-		0+(YPos_SCR)+A2_YT,
-		XPos_SCR+A2_XL-1,
-		0+(YPos_SCR)+A2_YB,
+		XPos_SCR+A2_XR,
+		0+(YPos_SCR)+A2_YT-1,
+		XPos_SCR+A2_XL,
+		0+(YPos_SCR)+A2_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 end
@@ -377,73 +458,144 @@ end
 --Clash Hitbox
 if ATKState > 0 and Facing == 1 then
 		gui.box(
-		XPos_SCR+C1_XR,
-		0+(YPos_SCR)+C1_YT,
-		XPos_SCR+C1_XL,
-		0+(YPos_SCR)+C1_YB,
+		XPos_SCR+C1_XR-1,
+		0+(YPos_SCR)+C1_YT-1,
+		XPos_SCR+C1_XL-1,
+		0+(YPos_SCR)+C1_YB-1,
 		0xFFFF0030,
 		0xFFFF00FF)
 elseif ATKState > 0 and Facing == 0 then
 		gui.box(
-		XPos_SCR+C1_XR-1,
-		0+(YPos_SCR)+C1_YT,
-		XPos_SCR+C1_XL-1,
-		0+(YPos_SCR)+C1_YB,
+		XPos_SCR+C1_XR,
+		0+(YPos_SCR)+C1_YT-1,
+		XPos_SCR+C1_XL,
+		0+(YPos_SCR)+C1_YB-1,
 		0xFFFF0030,
 		0xFFFF00FF)
 end
 
 gui.drawline((XPos_SCR - 3),YPos_SCR, (XPos_SCR + 3), YPos_SCR, 0xFFFFFFFF)
 gui.drawline(XPos_SCR,(-3+YPos_SCR), XPos_SCR, (3+YPos_SCR), 0xFFFFFFFF)
--------------------------------------
 
 -------------------------------------
 -- Draw DMW Boxes
 
 --Hitbox 1
-if Char == 06 then --Are you Alice?
+if Char == 6 then --Are you Alice?
 	if DMW_ATKState > 0 and DMW_Facing == 1 then
 		gui.box(
-		DMW_XPos_SCR+DMW_A1_XR,
-		0+(DMW_YPos_SCR)+DMW_A1_YT,
-		DMW_XPos_SCR+DMW_A1_XL,
-		0+(DMW_YPos_SCR)+DMW_A1_YB,
+		DMW_XPos_SCR+DMW_A1_XR-1,
+		0+(DMW_YPos_SCR)+DMW_A1_YT-1,
+		DMW_XPos_SCR+DMW_A1_XL-1,
+		0+(DMW_YPos_SCR)+DMW_A1_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
 		gui.box(
-		DMW_XPos_SCR+DMW_A1_XR-1,
-		0+(DMW_YPos_SCR)+DMW_A1_YT,
-		DMW_XPos_SCR+DMW_A1_XL-1,
-		0+(DMW_YPos_SCR)+DMW_A1_YB,
+		DMW_XPos_SCR+DMW_A1_XR,
+		0+(DMW_YPos_SCR)+DMW_A1_YT-1,
+		DMW_XPos_SCR+DMW_A1_XL,
+		0+(DMW_YPos_SCR)+DMW_A1_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 	end
 end
 
 --Hitbox 2
-if Char == 06 then --Are you Alice?
+if Char == 6 then --Are you Alice?
 	if DMW_ATKState > 0 and DMW_Facing == 1 then
 		gui.box(
-		DMW_XPos_SCR+DMW_A2_XR,
-		0+(DMW_YPos_SCR)+DMW_A2_YT,
-		DMW_XPos_SCR+DMW_A2_XL,
-		0+(DMW_YPos_SCR)+DMW_A2_YB,
+		DMW_XPos_SCR+DMW_A2_XR-1,
+		0+(DMW_YPos_SCR)+DMW_A2_YT-1,
+		DMW_XPos_SCR+DMW_A2_XL-1,
+		0+(DMW_YPos_SCR)+DMW_A2_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
 		gui.box(
-		DMW_XPos_SCR+DMW_A2_XR-1,
-		0+(DMW_YPos_SCR)+DMW_A2_YT,
-		DMW_XPos_SCR+DMW_A2_XL-1,
-		0+(DMW_YPos_SCR)+DMW_A2_YB,
+		DMW_XPos_SCR+DMW_A2_XR,
+		0+(DMW_YPos_SCR)+DMW_A2_YT-1,
+		DMW_XPos_SCR+DMW_A2_XL,
+		0+(DMW_YPos_SCR)+DMW_A2_YB-1,
 		0xFF000030,
 		0xFF0000FF)
 	end
 end
 
 --DMW Axis
-if Char == 0x06 then --Are you Alice?
+if Char == 6 then --Are you Alice?
+gui.drawline((DMW_XPos_SCR - 3),DMW_YPos_SCR, (DMW_XPos_SCR + 3), DMW_YPos_SCR, 0xFFFFFFFF)
+gui.drawline(DMW_XPos_SCR,(-3+DMW_YPos_SCR), DMW_XPos_SCR, (3+DMW_YPos_SCR), 0xFFFFFFFF)
+end
+
+-------------------------------------
+-- Draw Afterimage Boxes
+
+--Hitbox 1
+if (Char == 0 or Char == 3 or Char == 8) and Boost == 1 then --Are you Alice?
+	if DMW_ATKState > 0 and DMW_Facing == 1 then
+		gui.box(
+		DMW_XPos_SCR+DMW_A1_XR-1,
+		0+(DMW_YPos_SCR)+DMW_A1_YT-1,
+		DMW_XPos_SCR+DMW_A1_XL-1,
+		0+(DMW_YPos_SCR)+DMW_A1_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
+		gui.box(
+		DMW_XPos_SCR+DMW_A1_XR,
+		0+(DMW_YPos_SCR)+DMW_A1_YT-1,
+		DMW_XPos_SCR+DMW_A1_XL,
+		0+(DMW_YPos_SCR)+DMW_A1_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	end
+end
+
+--Hitbox 2
+if (Char == 0 or Char == 3 or Char == 8) and Boost == 1 then --Are you Alice?
+	if DMW_ATKState > 0 and DMW_Facing == 1 then
+		gui.box(
+		DMW_XPos_SCR+DMW_A2_XR-1,
+		0+(DMW_YPos_SCR)+DMW_A2_YT-1,
+		DMW_XPos_SCR+DMW_A2_XL-1,
+		0+(DMW_YPos_SCR)+DMW_A2_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
+		gui.box(
+		DMW_XPos_SCR+DMW_A2_XR,
+		0+(DMW_YPos_SCR)+DMW_A2_YT-1,
+		DMW_XPos_SCR+DMW_A2_XL,
+		0+(DMW_YPos_SCR)+DMW_A2_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	end
+end
+
+--Clashbox
+if (Char == 0 or Char == 3 or Char == 8) and Boost == 1 then --Are you Alice?
+	if DMW_ATKState > 0 and DMW_Facing == 1 then
+		gui.box(
+		DMW_XPos_SCR+DMW_C1_XR-1,
+		0+(DMW_YPos_SCR)+DMW_C1_YT-1,
+		DMW_XPos_SCR+DMW_C1_XL-1,
+		0+(DMW_YPos_SCR)+DMW_C1_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	elseif DMW_ATKState > 0 and DMW_Facing == 0 then
+		gui.box(
+		DMW_XPos_SCR+DMW_C1_XR,
+		0+(DMW_YPos_SCR)+DMW_C1_YT-1,
+		DMW_XPos_SCR+DMW_C1_XL,
+		0+(DMW_YPos_SCR)+DMW_C1_YB-1,
+		0xFF000030,
+		0xFF0000FF)
+	end
+end
+
+--Afterimage Axis
+if (Char == 0 or Char == 3 or Char == 8) and Boost == 1 then --Are you Alice?
 gui.drawline((DMW_XPos_SCR - 3),DMW_YPos_SCR, (DMW_XPos_SCR + 3), DMW_YPos_SCR, 0xFFFFFFFF)
 gui.drawline(DMW_XPos_SCR,(-3+DMW_YPos_SCR), DMW_XPos_SCR, (3+DMW_YPos_SCR), 0xFFFFFFFF)
 end
